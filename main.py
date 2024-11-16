72be71012d22b6767aac0b9d60e613ae9842b256
jrc <jrc@josericardocuadra.com>
2024-11-15 22:55:35 -0500

    final push

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ class Stack:
         # Initialize an empty list to store stack items
         self.items = []
         return
+    
+    def getSize(self):
+        # Return stack size
+        return len(self.items)
 
     def is_empty(self):
         # Return True if the stack is empty, otherwise False
@@ -81,6 +85,10 @@ print(is_balanced(expression4))
 
 
 ''' CALCULATOR '''
+''' Jose: Been trying to figure out how to make the calculator functions work for over 5 hours now.
+I gave it my all, can't seem to figure out exactly what is wrong and how to fix it. I know it is somewhere
+between the apply_operator function and the combo of ["elif char == ')':" and "elif char in "+-*/":"].
+Honestly, not sure exactly how to fix it. Uploading -- hoping for the best. (15/11/24)'''
 
 
 
@@ -94,7 +102,34 @@ def evaluate_expression(expression):
         # Pop the top operator from op_stack
         # Pop the top two numbers from num_stack
         # Perform the operation and push the result back to num_stack
-        
+        operator = op_stack.pop()
+        num1 = num_stack.pop()
+        print(num1)
+        num1 = float(num1)
+        num2 = num_stack.pop()
+        print(num2)
+        if num2 is None:
+            op_stack.push(operator)
+            num_stack.push(num1)
+            return
+        else:
+            num2 = float(num2)
+            if operator == '*':
+                num = num1 * num2
+                num_stack.push(str(num))
+                return
+            elif operator == '/':
+                num = num1 / num2
+                num_stack.push(str(num))
+                return
+            elif operator == '+':
+                num = num1 + num2
+                num_stack.push(str(num))
+                return
+            else:
+                num = num1 - num2
+                num_stack.push(str(num))
+                return
 
     # Loop through each character in the expression
     i = 0
@@ -109,22 +144,51 @@ def evaluate_expression(expression):
         # Check if the character is a digit
         if char.isdigit():
             # Parse the entire number and push it to num_stack
-            
+            num = ''
+            # Continue to accumulate digits for multi-digit numbers
+            while i < len(expression) and expression[i].isdigit():
+                num += expression[i]
+                i += 1
+            num_stack.push(num)  # Push the entire number to num_stack
+            continue  # Skip the increment at the end, since we've already moved the index
 
         # Check if the character is an opening parenthesis '('
         elif char == '(':
             # Push '(' to op_stack to mark the start of a group
-            
+            op_stack.push(char)
 
         # Check if the character is a closing parenthesis ')'
         elif char == ')':
             # Pop and apply operators until '(' is found
-           
+            find = op_stack.pop()
+            while find != '(':
+               find = op_stack.pop()
 
         # If it's an operator (+, -, *, /)
         elif char in "+-*/":
             # Apply operators based on precedence, then push current operator to op_stack
-            
+            op_stack.push(char)
+            '''num1 = num_stack.pop()
+            num1 = float(num1)
+            num2 = num_stack.pop()
+            if num2 is None:
+                num_stack.push(num1)
+                op_stack.push(char)
+            else:
+                num2 = float(num2)
+                if char == '*':
+                    num = num1 * num2
+                    num_stack.push(str(num))
+                elif char == '/':
+                    num = num1 / num2
+                    num_stack.push(str(num))
+                elif char == '+':
+                    num = num1 + num2
+                    num_stack.push(str(num))
+                else:
+                    num = num1 - num2
+                    num_stack.push(str(num))'''     
+
         # Move to the next character
         i += 1
 
